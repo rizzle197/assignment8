5c472bc19e46c472368719d40729f3cfb05afb42
rizzle197 <connorgibson986@gmail.com>
2025-10-25 04:33:34 -0400

    added button fill for user selected option by utilizing the ".selected" class and ability to track user answers in userAnswers object

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 console.log("script.js connected!");
+
+let userAnswers = {};   //Setting up key, value pairs for each user answer to the questions
+
+// Selecting all question blocks
+let questions = document.querySelectorAll(".question-block");
+
+questions.forEach(function(questionBlock) {
+    // Selecting all answer buttons in question block
+    let buttons = questionBlock.querySelectorAll(".answer-btn");
+
+    buttons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            // Removing "selected" class from the buttons in block
+            buttons.forEach(function(btn) {
+                btn.classList.remove("selected");
+            });
+
+            // Adding "selected" class to the button user clicks
+            button.classList.add("selected")
+
+            //Storing user answers in "userAnswers" variable
+            let questionID = button.dataset.answer;    // Decides which question it's on based on "data-answer" in html
+            let points = parseInt(button.dataset.points);   // How many points are given for each question choice
+            userAnswers[questionID] = points
+
+            console.log(userAnswers);   // Shows answers in the console
+        });
+    });
+});
